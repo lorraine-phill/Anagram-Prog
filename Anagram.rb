30b6de9782c4313b2ac26ce9419001f703b9148b
lorraine-phill <lp3767-web@yahoo.com>
2013-10-05 13:28:10 +0100

    Update Anagram.rb

--- Anagram.rb
+++ Anagram.rb
@@ -1,7 +1,4 @@
-=begin
-    You're given a list of strings, return a list of list of strings of anagrams, .e. each element of the returned list is a list of words that are anagrams among them.
-    For example: input   [stars, mary, rats, tars, army, banana] and output: [ [rats, tars], [army, mary], [stars], [banana] ].
-=end
+
 
 input = ["stars", "mary", "tars", "rats", "army", "banana"]
 result = [] # create a container 
